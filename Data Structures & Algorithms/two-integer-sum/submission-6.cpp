a5ad4f85aaa12fd098ae97c08ef155db62709f68
Rakib Hasan <mrh803705@gmail.com>
2026-09-17 16:31:55 +0600

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      
+    int n = nums.size();        
+
+    for(int i = 0 ; i< n ; i++){
+        int result = target - nums[i];
+
+        for(int j = 0 ; j<n ; j++){
+            if(i == j) continue ; 
+            if(nums[j]==result)return {i,j};    
+
+        }
+
+
+    }
+      
+
+
+
+
+
+    }
+};
+
+//for [3,3,4]
+//target 7 our output is [1,2] but the output should be [0,2] 
+//for this case this solution will not work
